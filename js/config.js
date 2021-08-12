c8998465687a38dbecf6481ce355665f3af8889b
dailalung10 <dailalung@gmail.com>
2021-08-12 22:45:41 +0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào cậu Lê Xuân Đức!',
+    introDesc: `Ơ kìa nhìn cái gì thế ?`,
+    btnIntro: 'Nhấn đây nè',
+    title: 'Hôm nay nắng quá nhỉ ',
+    desc: 'Cậu có muốn làm ny tớ không?',
+    btnYes: 'Có ạ',
+    btnNo: 'Đéo nhé',
+    question:'Đồng ý nhanh thế á? Tại sao lại nhấn có???',
+    btnReply: 'Trả lời ở đây',
+    reply: 'Yêu thì yêu không yêu thì yêu <33333333',
+    mess: 'Cảm ơn cậu',
+    messDesc: 'Im just kidding...',
+    btnAccept: 'Vâng <3',
+    messLink: 'https://www.facebook.com/dailalung10' 
+}
